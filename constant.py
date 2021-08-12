57ccb10bc132dd34925a5448f6cb18a0989d7539
Sebastian Villegas Zapata <sebastian.villegasz@perficient.com>
2021-08-12 11:21:31 -0500

    Updated: constants with latest data. Scheduler  to UTC

--- constant.py
+++ constant.py
@@ -4,59 +4,59 @@ CITIES = {
     'ARMENIA': {
         'value': '63001',
         'entities': {
-            'CIRCUITO': '3-True-3103-63001-Juzgado de Circuito-Civil',
-            'MUNICIPAL': '335-True-4003-63001-Juzgado Municipal-Civil'
+            'CIRCUITO': '3-True-3103-63001-JUZGADO DE CIRCUITO-CIVIL',
+            'MUNICIPAL': '335-True-4003-63001-JUZGADO MUNICIPAL-CIVIL'
         }
     },
     'BELLO': {
         'value': '05088',
         'entities': {
-            'MUNICIPAL': '570-True-4003-05088-Juzgado Municipal-Civil'
+            'MUNICIPAL': '570-True-4003-05088-JUZGADO MUNICIPAL-CIVIL'
         }
     },
     'BOGOTA': {
         'value': '11001',
         'entities': {
-            'CIRCUITO': '397-True-3103-11001-Juzgado de Circuito-Civil'
+            'CIRCUITO': '397-True-3103-11001-JUZGADO DE CIRCUITO-CIVIL'
         }
     },
     'ENVIGADO': {
         'value': '05266',
         'entities': {
-            'CIRCUITO': '279-True-3103-05266-Juzgado de Circuito-Civil',
-            'MUNICIPAL': '379-True-4003-05266-Juzgado Municipal-Civil'
+            'CIRCUITO': '279-True-3103-05266-JUZGADO DE CIRCUITO-CIVIL',
+            'MUNICIPAL': '379-True-4003-05266-JUZGADO MUNICIPAL-CIVIL'
         }
     },
     'ITAGUI': {
         'value': '05360',
         'entities': {
-            'CIRCUITO': '278-True-3103-05360-Juzgado de Circuito-Civil',
-            'MUNICIPAL': '105-True-4003-05360-Juzgado Municipal-Civil'
+            'CIRCUITO': '278-True-3103-05360-JUZGADO DE CIRCUITO-CIVIL',
+            'MUNICIPAL': '105-True-4003-05360-JUZGADO MUNICIPAL-CIVIL'
         }
     },
     'MEDELLIN': {
         'value': '05001',
         'entities': OrderedDict([
-            ('FAMILIA', '121-True-3110-05001-Juzgado de Circuito-Familia'),
-            ('TRIBUNAL ADMINISTRATIVO DE ANTIOQUIA', '463-True-2333-05001-Tribunal Administrativo-Oralidad'),
-            ('TRIBUNAL SUPERIOR DE ANTIOQUIA', '117-True-2213-05000-Tribunal Superior-Sala Civil-Familia'),
-            ('TRIBUNAL SUPERIOR', '113-True-2203-05001-Tribunal Superior-Civil'),
-            ('MUNICIPAL', '119-True-4003-05001-Juzgado Municipal-Civil'),
-            ('CIRCUITO', '118-True-3103-05001-Juzgado de Circuito-Civil'),
-            ('ADMINISTRATIVO', '462-True-3333-05001-Juzgado Administrativo-Oralidad')
+            ('FAMILIA', '121-True-3110-05001-JUZGADO DE CIRCUITO-FAMILIA'),
+            ('TRIBUNAL ADMINISTRATIVO DE ANTIOQUIA', '463-True-2333-05001-TRIBUNAL ADMINISTRATIVO-SIN SECCIÓN - ORAL'),
+            ('TRIBUNAL SUPERIOR DE ANTIOQUIA', '117-True-2213-05000-TRIBUNAL SUPERIOR-CIVIL - FAMILIA'),
+            ('TRIBUNAL SUPERIOR', '113-True-2203-05001-TRIBUNAL SUPERIOR-CIVIL'),
+            ('MUNICIPAL', '119-True-4003-05001-JUZGADO MUNICIPAL-CIVIL'),
+            ('CIRCUITO', '118-True-3103-05001-JUZGADO DE CIRCUITO-CIVIL'),
+            ('ADMINISTRATIVO', '462-True-3333-05001-JUZGADO ADMINISTRATIVO-SIN SECCIÓN - ORAL')
         ])
     },
     'PEREIRA': {
         'value': '66001',
         'entities': {
-            'CIRCUITO': '291-True-3103-66001-Juzgado de Circuito-Civil',
-            'MUNICIPAL': '293-True-4003-66001-Juzgado Municipal-Civil'
+            'CIRCUITO': '291-True-3103-66001-JUZGADO DE CIRCUITO-CIVIL',
+            'MUNICIPAL': '293-True-4003-66001-JUZGADO MUNICIPAL-CIVIL'
         }
     },
     'RIONEGRO': {
         'value': '05615',
         'entities': {
-            'CIRCUITO': '252-True-3103-05615-Juzgado de Circuito-Civil'
+            'CIRCUITO': '252-True-3103-05615-JUZGADO DE CIRCUITO-CIVIL'
         }
     }
 }
